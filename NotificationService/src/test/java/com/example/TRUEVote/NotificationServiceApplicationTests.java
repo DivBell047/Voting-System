634aaa9f0811288301f7775e5054b93612e2d1a1
rrb115 <113207930+rrb115@users.noreply.github.com>
2025-03-05 17:08:46 +0530

    NotificationService implemented

---
+++ NotificationService/src/test/java/com/example/TRUEVote/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TRUEVote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
